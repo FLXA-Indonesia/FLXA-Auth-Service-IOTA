f695b6913991d649ddcfdc7870a1fd600573aad1
ahmadzakiakmal <ahmad.zaki.akmal@gmail.com>
2025-05-03 12:00:48 +0700

    fix: handle ssl

--- initializers/connect-db.go
+++ initializers/connect-db.go
@@ -13,6 +13,10 @@ var DB *gorm.DB
 func ConnectDB() {
 	var err error
 	dsn := os.Getenv("DB_CONN_STRING")
+
+	// Ensure sslmode=require is in the connection string
+	dsn += " sslmode=require"
+
 	DB, err = gorm.Open(postgres.Open(dsn))
 
 	if err != nil {
